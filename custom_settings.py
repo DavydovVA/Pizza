f5bd6c938d4e89c53c17b80ad1e1dee7c09e0771
DavydovVA <volerej@gmail.com>
2020-02-13 17:49:50 +0300

    updated .gitignore

--- custom_settings.py
+++
@@ -1,7 +0,0 @@
-service_user_email = 'paimvsmind@mail.ru'
-service_password_email = '7metro21078'
-
-su_email = 'volerej@gmail.com'
-su_first_name = 'Valery'
-su_last_name = 'Davydov'
-su_password = 'admin'
